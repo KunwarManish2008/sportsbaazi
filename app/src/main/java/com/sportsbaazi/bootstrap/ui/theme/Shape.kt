2678de4eddea5a86b124024d7e9b5efccd23c5dc
Manish Kumar <manish.k@pepstudy.com>
2024-05-18 10:20:42 +0530

    Initial commit.

---
+++ app/src/main/java/com/sportsbaazi/bootstrap/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.sportsbaazi.bootstrap.ui.theme
+
+/**
+ * Created by Manish Kumar on 17/05/24.
+ */
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val SportsBaaziShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(8.dp)
+)
